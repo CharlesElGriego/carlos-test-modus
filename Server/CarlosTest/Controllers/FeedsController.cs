5434cec887fe8b8ee9b9ca07fa750029c2097325
Carlos Navarro <carlos.navarro@eimprovement.com>
2020-04-09 23:12:23 -0600

    Feat(Controllers): Add Feeds endpoints

--- Server/CarlosTest/Controllers/FeedsController.cs
+++ Server/CarlosTest/Controllers/FeedsController.cs
@@ -1,9 +1,12 @@
-﻿using CarlosTest.Services;
+﻿using CarlosTest.Dtos;
+using CarlosTest.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 
 namespace CarlosTest.Controllers
@@ -11,38 +14,79 @@ namespace CarlosTest.Controllers
     [RoutePrefix("api/feeds")]
     public class FeedsController : ApiController
     {
+        #region Private Properties
         private readonly IFeedService _feedService;
+        #endregion
 
+        #region Constructor
         public FeedsController(IFeedService feedService) : base()
         {
             _feedService = feedService;
         }
 
-        // GET: api/Feeds
-        public IHttpActionResult Get()
+        #endregion
+
+        #region Public Methods
+        [HttpGet]
+        public async Task<IHttpActionResult> Get()
         {
-            return Json(_feedService.GetFeeds());
+            return Json(await _feedService.GetFeeds());
         }
 
-        // GET: api/Feeds/5
-        public string Get(int id)
+        [Authorize]
+        [HttpGet]
+        [Route("GetFeedItems")]
+        public async Task<IHttpActionResult> GetFeedItems(int feedId)
         {
-            return "value";
+            return Json(await _feedService.GetFeedItems(feedId));
         }
 
-        // POST: api/Feeds
-        public void Post([FromBody]string value)
+        [Authorize]
+        [HttpGet]
+        [Route("GetFeedItems")]
+        public async Task<IHttpActionResult> GetFeedItems(string email)
         {
+            return Json(await _feedService.GetFeedItems(email));
         }
 
-        // PUT: api/Feeds/5
-        public void Put(int id, [FromBody]string value)
+        [Authorize]
+        [HttpPost]
+        [Route("suscribetofeed")]
+        public async Task<IHttpActionResult> SuscribedToFeed([FromBody] UserFeedDto userFeed )
         {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            try
+            {
+                var suscribe = await _feedService.SuscribeToFeed(userFeed.FeedId, userFeed.UserEmail);
+
+                if (!suscribe)
+                {
+                    throw  new HttpResponseException(HttpStatusCode.BadRequest);
+                }
+                
+            }
+            catch (HttpResponseException exception)
+            {
+               int httpStatusCode = (int)exception.Response.StatusCode;
+
+                if (httpStatusCode == (int)HttpStatusCode.BadRequest)
+                {
+                    HttpResponseMessage errorMessage = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    errorMessage.Content = new StringContent("Wrong data");
+                    throw new HttpResponseException(errorMessage);
+                }
+                else if (httpStatusCode == (int)HttpStatusCode.Conflict)
+                {
+                    HttpResponseMessage errorMessage = new HttpResponseMessage(HttpStatusCode.Conflict);
+                    errorMessage.Content = new StringContent("User is already subscribed to this feed");
+                    throw new HttpResponseException(errorMessage);
+                }
+            }
+            return Ok();
         }
 
-        // DELETE: api/Feeds/5
-        public void Delete(int id)
-        {
-        }
+        #endregion
     }
 }
