93c13134f4b674177f4ef437b10040dfeb020874
Carlos Navarro <carlos.navarro@eimprovement.com>
2020-04-10 18:54:40 -0600

    feat: add login

---
+++ Client-App/src/app/security/login/login.component.ts
@@ -0,0 +1,52 @@
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginComponent implements OnInit, OnDestroy {
+
+  //#region   Public Properties
+  loginForm = this.fb.group({
+    username: ['', Validators.compose([Validators.required, Validators.email])],
+    password: ['', Validators.required],
+  });
+  //#endregion
+
+  //#region   Constructor
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder
+  ) { }
+  //#region
+
+  /*Lifecycle Hooks  */
+  ngOnInit(): void {
+  }
+  ngOnDestroy(): void {
+  }
+
+  //#region   Public Methods
+  login(): void {
+    this.authService.login(this.loginForm.value).subscribe(token => {
+      if (token) {
+        this.authService.saveToken(token);
+        this.authService.authenticated$.next(true);
+        this.authService.setUser();
+      }
+    });
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+  //#endregion
+
+}
