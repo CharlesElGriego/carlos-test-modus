e2770986817cbbee5affd4c004f78ffd2765fa29
Carlos Navarro <carlos.navarro@eimprovement.com>
2020-04-10 18:53:29 -0600

    feat(app): add default layout shell

--- Client-App/src/app/app.component.ts
+++ Client-App/src/app/app.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
-
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
+import { AuthService } from './security/auth.service';
+import { ErrorHandlerService } from './security/error-handler.service';
+import { User } from './security/models';
+import { SnackbarService } from './shared/services/snackbar.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'carlos-news-feed';
+export class AppComponent implements OnInit, OnDestroy {
+
+  //#region   Public Properties
+  mobileQuery: MediaQueryList;
+  navList = [
+    {
+      link: 'feeds',
+      name: 'Feeds',
+      exact: true,
+    },
+    {
+      link: 'my-feeds',
+      name: 'My Feeds',
+      exact: true,
+    },
+    {
+      link: 'feeds',
+      name: 'My News',
+      exact: true,
+    },
+  ];
+  title = 'Carlos News Feeds';
+  //#endregion
+
+  //#region   Observables
+  authenticated$: Observable<boolean>;
+  user$: Observable<User>;
+  //#endregion
+
+  //#region   Private Properties
+  private alive = true;
+  private mobileQueryListener: () => void;
+  //#endregion
+
+  //#region   Constructor
+  constructor(
+    private authService: AuthService,
+    private changeDetectorRef: ChangeDetectorRef,
+    private errorHandlerService: ErrorHandlerService,
+    private snackbarService: SnackbarService,
+    private media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this.mobileQueryListener);
+  }
+  //#endregion
+
+  //#region   Public Methods
+  ngOnInit(): void {
+    this.registerEvents();
+  }
+  ngOnDestroy(): void {
+    this.alive = false;
+  }
+  //#endregion
+
+  //#region   Private Methods
+  private registerEvents(): void {
+    this.authenticated$ = this.authService.authenticated$;
+    this.user$ = this.authService.user$;
+
+    this.errorHandlerService.serverError$
+      .pipe(takeWhile(() => this.alive))
+      .subscribe(message => {
+        if (message) {
+          this.snackbarService.showError(message);
+        }
+      });
+  }
 }
