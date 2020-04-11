cd036052041989099cf5f029f9b2ab062a37ab15
Carlos Navarro <carlos.navarro@eimprovement.com>
2020-04-10 20:04:02 -0600

    feat(views): add feeds

---
+++ Client-App/src/app/views/feeds/feeds.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FeedsComponent } from './feeds/feeds.component';
+
+
+
+@NgModule({
+  declarations: [FeedsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FeedsModule { }
