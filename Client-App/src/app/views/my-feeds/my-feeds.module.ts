ba69696a98ff088e18974870e0bca8e04d4d77e3
Carlos Navarro <carlos.navarro@eimprovement.com>
2020-04-10 20:04:16 -0600

    feat(views): add my feeds

---
+++ Client-App/src/app/views/my-feeds/my-feeds.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MyFeedsComponent } from './my-feeds/my-feeds.component';
+
+
+
+@NgModule({
+  declarations: [MyFeedsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MyFeedsModule { }
---
+++ Client-App/src/app/views/my-feeds/my-feeds/my-feeds.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-feeds',
+  templateUrl: './my-feeds.component.html',
+  styleUrls: ['./my-feeds.component.scss']
+})
+export class MyFeedsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
