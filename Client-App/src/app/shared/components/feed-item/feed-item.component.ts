83fb5d64375d86582835f28caf235c5bd6a2b5ef
Carlos Navarro <carlos.navarro@eimprovement.com>
2020-04-13 02:18:24 -0600

    feat(shared): add feed item component

--- Client-App/src/app/shared/components/feed-item/feed-item.component.ts
+++ Client-App/src/app/shared/components/feed-item/feed-item.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { FeedItem } from '../../models';
 
 @Component({
   selector: 'app-feed-item',
   templateUrl: './feed-item.component.html',
-  styleUrls: ['./feed-item.component.scss']
+  styleUrls: ['./feed-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FeedItemComponent implements OnInit {
 
-  constructor() { }
+  //#region   Public Properties
+  @Input() feedItem: FeedItem;
+  //#endregion
 
-  ngOnInit() {
+  //#region   Constructor
+  constructor() { }
+  //#endregion
+
+  //#region   Lifecycle hooks
+  ngOnInit(): void {
   }
+  //#endregion
 
 }
